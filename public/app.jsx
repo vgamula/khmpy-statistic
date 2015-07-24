e7135cc15f7398cddc1b00d499f125ea2a1cee2f
Volodymyr Gamula <v.gamula@gmail.com>
2015-07-25 00:25:39 +0300

    Base functionallity

--- public/app.jsx
+++ public/app.jsx
@@ -1,12 +1,50 @@
 /* global React, io */
 
+var Row = React.createClass({
+    render() {
+        var obj = this.props.obj;
+        return (
+            <li>
+                <h4>{obj.title}</h4>
+                <p>+: {obj.plus}</p>
+                <p>-: {obj.minus}</p>
+                <p>all: {obj.amount}</p>
+                <button onClick={this.vote}>Vote</button>
+                <hr />
+            </li>
+        );
+    },
+    vote: function() {
+        this.props.socket.emit('vote', this.props.obj.id);
+    }
+});
+
 var App = React.createClass({
+    getInitialState: function() {
+        return {};
+    },
     render: function() {
+        var self = this;
         return (
             <div>
-                Hello World!
+                <p>Topics list!</p>
+                <ul>
+                    {this.state.data && this.state.data.map(function(obj) {
+                        return <Row obj={obj} socket={self.socket} />;
+                    })}
+                </ul>
             </div>
         );
+    },
+    componentDidMount: function() {
+        var self = this;
+        this.socket = io();
+        this.socket.on('data', function(data) {
+            console.log(data);
+            self.setState({
+                data: data
+            });
+        });
     }
 });
 
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var express = require('express');
+var _ = require('lodash');
 
 var app = express();
 app.use('/', express.static(path.join(__dirname, 'public')));
@@ -8,3 +9,68 @@ var server = app.listen(3000);
 console.log('Server listening on port 3000');
 
 var io = require('socket.io')(server);
+
+var globalData = [
+    {
+        id: 1,
+        title: 'first',
+        plus: [],
+        minus: []
+    },
+    {
+        id: 2,
+        title: 'second',
+        plus: [],
+        minus: []
+    },
+    {
+        id: 3,
+        title: 'third',
+        plus: [],
+        minus: []
+    },
+    {
+        id: 4,
+        title: 'fifth',
+        plus: [],
+        minus: []
+    }
+];
+
+function getData() {
+    return globalData.map(function(obj) {
+        return {
+            id: obj.id,
+            title: obj.title,
+            plus: obj.plus.length,
+            minus: obj.minus.length,
+            amount: obj.plus.length + obj.minus.length,
+        }
+    });
+}
+
+io.on('connection', function(socket) {
+    io.sockets.emit('data', getData());
+
+    socket.on('vote', function(dataId) {
+        var elem = _.findWhere(globalData, {id: dataId});
+        if (_.includes(elem.minus, socket)) {
+            _.remove(elem.minus, socket);
+            elem.plus.push(socket);
+        } else if (_.includes(elem.plus, socket)) {
+            _.remove(elem.plus, socket);
+            elem.minus.push(socket);
+        } else {
+            elem.plus.push(socket);
+        }
+        io.sockets.emit('data', getData());
+    });
+
+    socket.on('disconnect', function() {
+        globalData.map(function(obj) {
+            _.remove(obj.minus, socket);
+            _.remove(obj.plus, socket);
+        });
+        io.sockets.emit('data', getData());
+    });
+});
